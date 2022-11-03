f0d5edeedc5218e6903088d6767d3c60d8e35c1f
Trần Mậu Phi <phitmps16509@fpt.edu.vn>
2022-11-03 18:49:18 +0700

    update

---
+++ src/views/components/general/HomeChat/index.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ChatSmall from "../../general/ChatSmall";
+
+export default function HomeChat() {
+  return (
+    <React.Fragment>
+      <div className="heading">
+        <h4>Nhắn tin</h4> <i className="uil uil-edit"></i>
+      </div>
+      {/* <!------------------------------- Search bar ----------------------------> */}
+      <div className="search-bar">
+        <i className="uil uil-search"></i>
+        <input type="search" id="search-bar" placeholder="Tìm kiếm tin nhắn" />
+      </div>
+      {/* <!------------------------------- Messages ----------------------------> */}
+      <div className="all-messages">
+        <ChatSmall />
+        <ChatSmall />
+        <ChatSmall />
+        <ChatSmall />
+        <ChatSmall />
+        <ChatSmall />
+        <ChatSmall />
+        <ChatSmall />
+        <ChatSmall />
+      </div>
+    </React.Fragment>
+  );
+}
