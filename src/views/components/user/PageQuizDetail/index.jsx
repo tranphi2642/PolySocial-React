8bd077523952db931749543d70fa1505b247b394
Trần Mậu Phi <phitmps16509@fpt.edu.vn>
2022-11-03 18:57:36 +0700

    Revert "update"
    This reverts commit a6c27d7e14cde8d5213ce93c04b02728f6630228.

--- src/views/components/user/PageQuizDetail/index.jsx
+++ src/views/components/user/PageQuizDetail/index.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-import { UilExport } from "@iconscout/react-unicons";
+import { Link } from "react-router-dom";
+import {
+  UilHome,
+  UilUsersAlt,
+  UilFeedback,
+  UilFileUpload,
+  UilSignout,
+  UilEllipsisH,
+  UilLock,
+  UilUserCheck,
+  UilPlus,
+  UilExport,
+} from "@iconscout/react-unicons";
 import Nav from "../../general/Nav/index";
-import NavPage from "../../general/NavPage";
 
 import avatar from "../../../../assets/images/1.jpg";
 
@@ -12,24 +23,100 @@ export default function PageQuizDetail() {
       <main className="pages">
         <div className="container-page">
           <div className="left">
-            <NavPage />
+            <div className="background">
+              <img
+                src="https://www.facebook.com/images/groups/groups-default-cover-photo-2x.png"
+                alt=""
+              />
+            </div>
+            <div className="title">
+              <span>SYB.IT16307_3.B2.2022</span>
+              <p className="text-muted">
+                <i>
+                  <UilLock />
+                </i>{" "}
+                Nhóm riêng tư - <i>31 thành viên</i>
+              </p>
+            </div>
+            <div className="join">
+              <button>
+                <i>
+                  <UilUserCheck />
+                </i>{" "}
+                Đã tham gia
+              </button>
+              <button>
+                <i>
+                  <UilPlus />
+                </i>
+                Mời{" "}
+              </button>
+              <button>
+                <i>
+                  <UilEllipsisH />
+                </i>{" "}
+              </button>
+            </div>
+            {/* <!------------------------------- Side bar ----------------------------> */}
+            <div className="sidebar">
+              <Link to={"/pageDetail"} className="menu-item active">
+                <span>
+                  <i>
+                    <UilHome />
+                  </i>
+                </span>
+                <h3>Trang chủ</h3>
+              </Link>
+              <Link to={"/pagePeoples"} className="menu-item">
+                <span>
+                  <i>
+                    <UilUsersAlt />
+                  </i>
+                </span>
+                <h3>Thành viên</h3>
+              </Link>
+              <Link to={"/feedback"} className="menu-item">
+                <span>
+                  <i>
+                    <UilFeedback />
+                  </i>
+                </span>
+                <h3>Phản hồi</h3>
+              </Link>
+              <Link to={"/pageQuizs"} className="menu-item">
+                <span>
+                  <i>
+                    <UilFileUpload />
+                  </i>
+                </span>
+                <h3>Bài tập (Quiz)</h3>
+              </Link>
+              <Link to={"/page"} className="menu-item">
+                <span>
+                  <i>
+                    <UilSignout />
+                  </i>
+                </span>
+                <h3>Rời nhóm</h3>
+              </Link>
+            </div>
           </div>
           <div className="middle">
-            <div className="exercise">
-              <div className="profile">
-                <div className="profile-photo-1">
+            <div class="exercise">
+              <div class="profile">
+                <div class="profile-photo-1">
                   <img src={avatar} alt="" />
                 </div>
-                <div className="handle">
+                <div class="handle">
                   <h4>
                     Trần Phi<span> đã tạo mới một bài tập: Lab 1</span>
                   </h4>
-                  <p className="text-muted">28/09/2022</p>
+                  <p class="text-muted">28/09/2022</p>
                 </div>
 
-                <div className="wrapper">
+                <div class="wrapper">
                   <span>Chưa nộp</span>
-                  <div className="file-upload">
+                  <div class="file-upload">
                     <input type="file" />
                     <i>
                       <UilExport />
@@ -38,20 +125,20 @@ export default function PageQuizDetail() {
                 </div>
               </div>
 
-              <div className="profile">
-                <div className="profile-photo-1">
+              <div class="profile">
+                <div class="profile-photo-1">
                   <img src={avatar} alt="" />
                 </div>
-                <div className="handle">
+                <div class="handle">
                   <h4>
                     Trần Phi<span> đã tạo mới một bài tập: Lab 2</span>
                   </h4>
-                  <p className="text-muted">28/09/2022</p>
+                  <p class="text-muted">28/09/2022</p>
                 </div>
 
-                <div className="wrapper">
+                <div class="wrapper">
                   <span>Chưa nộp</span>
-                  <div className="file-upload">
+                  <div class="file-upload">
                     <input type="file" />
                     <i>
                       <UilExport />
@@ -60,20 +147,20 @@ export default function PageQuizDetail() {
                 </div>
               </div>
 
-              <div className="profile">
-                <div className="profile-photo-1">
+              <div class="profile">
+                <div class="profile-photo-1">
                   <img src={avatar} alt="" />
                 </div>
-                <div className="handle">
+                <div class="handle">
                   <h4>
                     Trần Phi<span> đã tạo mới một bài tập: Lab 3</span>
                   </h4>
-                  <p className="text-muted">28/09/2022</p>
+                  <p class="text-muted">28/09/2022</p>
                 </div>
 
-                <div className="wrapper">
+                <div class="wrapper">
                   <span>Chưa nộp</span>
-                  <div className="file-upload">
+                  <div class="file-upload">
                     <input type="file" />
                     <i>
                       <UilExport />
@@ -82,20 +169,20 @@ export default function PageQuizDetail() {
                 </div>
               </div>
 
-              <div className="profile">
-                <div className="profile-photo-1">
+              <div class="profile">
+                <div class="profile-photo-1">
                   <img src={avatar} alt="" />
                 </div>
-                <div className="handle">
+                <div class="handle">
                   <h4>
                     Trần Phi<span> đã tạo mới một bài tập: Lab 4</span>
                   </h4>
-                  <p className="text-muted">28/09/2022</p>
+                  <p class="text-muted">28/09/2022</p>
                 </div>
 
-                <div className="wrapper">
+                <div class="wrapper">
                   <span>Chưa nộp</span>
-                  <div className="file-upload">
+                  <div class="file-upload">
                     <input type="file" />
                     <i>
                       <UilExport />
@@ -104,20 +191,20 @@ export default function PageQuizDetail() {
                 </div>
               </div>
 
-              <div className="profile">
-                <div className="profile-photo-1">
+              <div class="profile">
+                <div class="profile-photo-1">
                   <img src={avatar} alt="" />
                 </div>
-                <div className="handle">
+                <div class="handle">
                   <h4>
                     Trần Phi<span> đã tạo mới một bài tập: Lab 5</span>
                   </h4>
-                  <p className="text-muted">28/09/2022</p>
+                  <p class="text-muted">28/09/2022</p>
                 </div>
 
-                <div className="wrapper">
+                <div class="wrapper">
                   <span>Chưa nộp</span>
-                  <div className="file-upload">
+                  <div class="file-upload">
                     <input type="file" />
                     <i>
                       <UilExport />
@@ -126,20 +213,20 @@ export default function PageQuizDetail() {
                 </div>
               </div>
 
-              <div className="profile">
-                <div className="profile-photo-1">
+              <div class="profile">
+                <div class="profile-photo-1">
                   <img src={avatar} alt="" />
                 </div>
-                <div className="handle">
+                <div class="handle">
                   <h4>
                     Trần Phi<span> đã tạo mới một bài tập: Lab 6</span>
                   </h4>
-                  <p className="text-muted">28/09/2022</p>
+                  <p class="text-muted">28/09/2022</p>
                 </div>
 
-                <div className="wrapper">
+                <div class="wrapper">
                   <span>Chưa nộp</span>
-                  <div className="file-upload">
+                  <div class="file-upload">
                     <input type="file" />
                     <i>
                       <UilExport />
@@ -148,20 +235,20 @@ export default function PageQuizDetail() {
                 </div>
               </div>
 
-              <div className="profile">
-                <div className="profile-photo-1">
+              <div class="profile">
+                <div class="profile-photo-1">
                   <img src={avatar} alt="" />
                 </div>
-                <div className="handle">
+                <div class="handle">
                   <h4>
                     Trần Phi<span> đã tạo mới một bài tập: Lab 7</span>
                   </h4>
-                  <p className="text-muted">28/09/2022</p>
+                  <p class="text-muted">28/09/2022</p>
                 </div>
 
-                <div className="wrapper">
+                <div class="wrapper">
                   <span>Chưa nộp</span>
-                  <div className="file-upload">
+                  <div class="file-upload">
                     <input type="file" />
                     <i>
                       <UilExport />
@@ -170,20 +257,20 @@ export default function PageQuizDetail() {
                 </div>
               </div>
 
-              <div className="profile">
-                <div className="profile-photo-1">
+              <div class="profile">
+                <div class="profile-photo-1">
                   <img src={avatar} alt="" />
                 </div>
-                <div className="handle">
+                <div class="handle">
                   <h4>
                     Trần Phi<span> đã tạo mới một bài tập: Lab 8</span>
                   </h4>
-                  <p className="text-muted">28/09/2022</p>
+                  <p class="text-muted">28/09/2022</p>
                 </div>
 
-                <div className="wrapper">
+                <div class="wrapper">
                   <span>Chưa nộp</span>
-                  <div className="file-upload">
+                  <div class="file-upload">
                     <input type="file" />
                     <i>
                       <UilExport />
@@ -192,20 +279,20 @@ export default function PageQuizDetail() {
                 </div>
               </div>
 
-              <div className="profile">
-                <div className="profile-photo-1">
+              <div class="profile">
+                <div class="profile-photo-1">
                   <img src={avatar} alt="" />
                 </div>
-                <div className="handle">
+                <div class="handle">
                   <h4>
                     Trần Phi<span> đã tạo mới một bài tập: Assignment GD1</span>
                   </h4>
-                  <p className="text-muted">28/09/2022</p>
+                  <p class="text-muted">28/09/2022</p>
                 </div>
 
-                <div className="wrapper">
+                <div class="wrapper">
                   <span>Chưa nộp</span>
-                  <div className="file-upload">
+                  <div class="file-upload">
                     <input type="file" />
                     <i>
                       <UilExport />
