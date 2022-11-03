965aeed0c6adfbb996f5e05c5ce29b253048f25b
Trần Mậu Phi <phitmps16509@fpt.edu.vn>
2022-11-03 19:03:45 +0700

    update

--- src/views/components/user/Home/index.jsx
+++ src/views/components/user/Home/index.jsx
@@ -21,7 +21,7 @@ import {
 
 import "./index.scss";
 import AddFriend from "../../general/ConfirmFriend";
-import ChatSmall from "../../general/ChatSmall";
+import HomeChat from "../../general/HomeChat";
 
 export default function Home() {
   const { account } = useLogin();
@@ -182,30 +182,7 @@ export default function Home() {
           {/* <!------------------------------- Right ----------------------------> */}
           <div className="right">
             <div className="messages">
-              <div className="heading">
-                <h4>Nhắn tin</h4> <i className="uil uil-edit"></i>
-              </div>
-              {/* <!------------------------------- Search bar ----------------------------> */}
-              <div className="search-bar">
-                <i className="uil uil-search"></i>
-                <input
-                  type="search"
-                  id="search-bar"
-                  placeholder="Tìm kiếm tin nhắn"
-                />
-              </div>
-              {/* <!------------------------------- Messages ----------------------------> */}
-              <div className="all-messages">
-                <ChatSmall />
-                <ChatSmall />
-                <ChatSmall />
-                <ChatSmall />
-                <ChatSmall />
-                <ChatSmall />
-                <ChatSmall />
-                <ChatSmall />
-                <ChatSmall />
-              </div>
+              <HomeChat />
             </div>
             {/* <!------------------------------- End Messages ----------------------------> */}
 
