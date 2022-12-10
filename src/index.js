7329dacd8a156e5ffbe43c27fff0da7228855202
Trần Mậu Phi <phitmps16509@fpt.edu.vn>
2022-12-10 16:34:39 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style/index.css";
-import App from "./App.js";
+import App from "./views/App.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
